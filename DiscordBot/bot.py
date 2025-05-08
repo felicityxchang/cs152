98cbd9b922e75bc42c39644b0712c68af56242ba
felicityxchang <fxchang@stanford.edu>
2025-05-08 11:13:53 -0700

    [fix] did not actually fix the weird race condition

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -133,10 +133,7 @@ class ModBot(discord.Client):
         for r in responses:
             await message.channel.send(r)
 
-        report = self.reports.get(author_id)
-        if report is None:
-            self.reports[author_id] = Report(self)
-            report = self.reports[author_id]
+        report = self.reports[author_id]
 
         if report.state == State.AWAITING_ADDITIONAL_SUICIDE_OPTIONS:
             # Check if this is a suicide/self-harm report + send to mods
